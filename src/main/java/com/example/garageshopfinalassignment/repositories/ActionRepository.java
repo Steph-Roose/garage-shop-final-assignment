301ae5384f8dcc35529af2884bbb515c4b2e31ad
Steph-Roose <steph241291@gmail.com>
2022-10-14 14:57:44 +0200

    added repositories

---
+++ src/main/java/com/example/garageshopfinalassignment/repositories/ActionRepository.java
@@ -0,0 +1,8 @@
+package com.example.garageshopfinalassignment.repositories;
+
+import com.example.garageshopfinalassignment.models.Action;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActionRepository extends JpaRepository<Action, Long> {
+
+}
---
+++ src/main/java/com/example/garageshopfinalassignment/repositories/LogRepository.java
@@ -0,0 +1,8 @@
+package com.example.garageshopfinalassignment.repositories;
+
+import com.example.garageshopfinalassignment.models.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogRepository extends JpaRepository<Log, Long> {
+
+}
